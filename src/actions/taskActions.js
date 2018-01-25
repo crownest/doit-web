ce35bfac154158c83697bb3accc80d0652697cd4
kenansubasi <kenansubasiceng@gmail.com>
2018-01-25 16:29:56 +0300

    TaskDeleteButton component updated.

--- src/actions/taskActions.js
+++ src/actions/taskActions.js
@@ -3,8 +3,7 @@ import {
   request,
   alertify,
   api_tasks_url,
-  getAuthInformations,
-  HTTP_204_NO_CONTENT,
+  getAuthInformations
 } from "./baseActions";
 
 
@@ -83,26 +82,15 @@ export function updateTask(task_id, data, onComplete) {
 }
 
 
-export function deleteTask(task_id) {
-  alertify.confirm("Are you sure you want to delete?", function () {
-    var auth_informations = getAuthInformations();
+export function deleteTask(task_id, onComplete) {
+  var auth_informations = getAuthInformations();
 
-    return request
-      .del(api_tasks_url + task_id + '/')
-      .set("Authorization", "TOKEN " + auth_informations.auth_token)
-      .type("application/json")
-      .accept("application/json")
-      .end((error, response) => {
-        if (response) {
-          if (response.statusCode === HTTP_204_NO_CONTENT) {
-            alertify.success("Task deleted.");
-            window.location.reload();
-          } else {
-            alertify.error("An unexpected error has occurred and try again later.");
-          }
-        } else {
-          alertify.error("An unexpected error has occurred and try again later.");
-        }
-      });
-  });
+  return request
+    .del(api_tasks_url + task_id + '/')
+    .set("Authorization", "TOKEN " + auth_informations.auth_token)
+    .type("application/json")
+    .accept("application/json")
+    .end((error, response) => {
+      onComplete(response);
+    });
 }
--- src/components/TaskDeleteButton/index.jsx
+++ src/components/TaskDeleteButton/index.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import SimpleLineIcon from 'react-simple-line-icons';
 
 // Actions
+import {
+  alertify,
+  HTTP_204_NO_CONTENT
+} from "../../actions/baseActions";
 import { deleteTask } from "../../actions/taskActions";
 
 // Local Modules
@@ -10,9 +14,21 @@ import "./index.css"
 
 
 export default class TaskDeleteButton extends React.Component {
-
   onDelete(task_id) {
-    deleteTask(task_id);
+    alertify.confirm("Are you sure you want to delete?", function () {
+      deleteTask(task_id, (response) => {
+        if (response) {
+          if (response.statusCode === HTTP_204_NO_CONTENT) {
+            alertify.success("Task deleted.");
+            window.location.reload()
+          } else {
+            alertify.error("An unexpected error has occurred and try again later.");
+          }
+        } else {
+          alertify.error("An unexpected error has occurred and try again later.");
+        }
+      });
+    });
   }
 
   render() {
